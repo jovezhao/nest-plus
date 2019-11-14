89f52c1caabdf6f2b1297151c3eb5fc49ec7c0c5
jovezhao <jovezhao@foxmail.com>
2019-11-14 14:30:58 +0800

    完成rockemq的测试

--- nest-plus-demo/src/main/java/com/zhaofujun/nest/demo/DemoConfiguration.java
+++ nest-plus-demo/src/main/java/com/zhaofujun/nest/demo/DemoConfiguration.java
@@ -3,8 +3,8 @@ package com.zhaofujun.nest.demo;
 import com.zhaofujun.nest.configuration.CacheConfiguration;
 import com.zhaofujun.nest.configuration.EventConfiguration;
 import com.zhaofujun.nest.context.event.store.DefaultMessageStore;
-import com.zhaofujun.nest.rabbitmq.RabbitMQMessageChannel;
 import com.zhaofujun.nest.redis.RedisCacheProvider;
+import com.zhaofujun.nest.rocketmq.RocketMQMessageChannel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,16 +15,16 @@ public class DemoConfiguration {
     public EventConfiguration testEventConfiguration() {
         EventConfiguration eventConfiguration = new EventConfiguration();
         eventConfiguration.setEventCode(DemoEventData.Code);
-        eventConfiguration.setMessageChannelCode(RabbitMQMessageChannel.CHANNEL_CODE);
+        eventConfiguration.setMessageChannelCode(RocketMQMessageChannel.CHANNEL_CODE);
         return eventConfiguration;
     }
 
-    @Bean
-    public CacheConfiguration cacheConfiguration() {
-        CacheConfiguration cacheConfiguration = new CacheConfiguration();
-        cacheConfiguration.setCacheCode(DefaultMessageStore.CACHE_CODE);
-        cacheConfiguration.setName("事件消息存储策略");
-        cacheConfiguration.setProviderCode(RedisCacheProvider.CODE);
-        return cacheConfiguration;
-    }
+//    @Bean
+//    public CacheConfiguration cacheConfiguration() {
+//        CacheConfiguration cacheConfiguration = new CacheConfiguration();
+//        cacheConfiguration.setCacheCode(DefaultMessageStore.CACHE_CODE);
+//        cacheConfiguration.setName("事件消息存储策略");
+//        cacheConfiguration.setProviderCode(RedisCacheProvider.CODE);
+//        return cacheConfiguration;
+//    }
 }

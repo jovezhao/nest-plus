dab64d0936c7ec665a0d03ce8814a4629bc69c15
jovezhao <jovezhao@foxmail.com>
2020-08-29 12:46:54 +0800

    将redis缓存存储方式由hash修改为key

--- nest-plus-spring-redis/src/main/java/com/zhaofujun/nest/redis/RedisHashCacheProvider.java
+++ nest-plus-spring-redis/src/main/java/com/zhaofujun/nest/redis/RedisHashCacheProvider.java
@@ -2,24 +2,22 @@ package com.zhaofujun.nest.redis;
 
 import com.zhaofujun.nest.cache.CacheProvider;
 import com.zhaofujun.nest.json.JsonCreator;
-import com.zhaofujun.nest.utils.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class RedisCacheProvider implements CacheProvider {
-    public final static String CODE = "REDIS_CACHE_PROVIDER";
+public class RedisHashCacheProvider implements CacheProvider {
+    public final static String CODE = "REDIS_HASH_CACHE_PROVIDER";
 
     private StringRedisTemplate redisTemplate;
     private JsonCreator jsonCreator;
 
-    public RedisCacheProvider(StringRedisTemplate redisTemplate) {
+    public RedisHashCacheProvider(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
-        this.jsonCreator = new JsonCreator();
+        this.jsonCreator=new JsonCreator();
     }
 
     @Override
@@ -27,22 +25,19 @@ public class RedisCacheProvider implements CacheProvider {
         return CODE;
     }
 
-    private String getRedisKey(String groupName, String key) {
-        return groupName + "_" + key;
-    }
-
     @Override
     public <T> T get(String groupName, String key, Type type) {
 
-        String redisKey = getRedisKey(groupName, key);
-        String json = redisTemplate.opsForValue().get(redisKey);
-        if (StringUtils.isEmpty(json)) return null;
-        return jsonCreator.toObj(json, type);
+        Object json = redisTemplate.opsForHash().get(groupName, key);
+        if (json != null)
+            return jsonCreator.toObj(json.toString(), type);
+        return null;
 
     }
 
     @Override
     public <T> Map<String, T> get(String groupName, Type type, String... keys) {
+
 
         Map<String, T> result = new HashMap<>();
 
@@ -60,32 +55,33 @@ public class RedisCacheProvider implements CacheProvider {
     public void put(String groupName, String key, Object value, long idleSeconds) {
         String json = jsonCreator.toJsonString(value);
 
-        String redisKey = getRedisKey(groupName, key);
-        redisTemplate.opsForValue().set(redisKey, json);
-        redisTemplate.expire(redisKey, idleSeconds, TimeUnit.SECONDS);
+        redisTemplate.opsForHash().put(groupName, key, json);
+        redisTemplate.expire(groupName,idleSeconds, TimeUnit.SECONDS);
     }
 
     @Override
     public boolean remove(String groupName, String key) {
-        String redisKey = getRedisKey(groupName, key);
-        return redisTemplate.delete(redisKey);
+
+        return redisTemplate.opsForHash().delete(groupName, key) > 0;
+
     }
 
     @Override
     public void removeAll(String groupName) {
 
-        Set<String> keys = redisTemplate.keys(groupName + "_*");
-        redisTemplate.delete(keys);
+        redisTemplate.delete(groupName);
     }
 
     @Override
     public boolean containsKey(String groupName, String key) {
-        String redisKey = getRedisKey(groupName, key);
-        return redisTemplate.hasKey(redisKey);
+
+        return redisTemplate.opsForHash().hasKey(groupName, key);
     }
 
     @Override
     public String[] getKeys(String groupName) {
-        return redisTemplate.keys(groupName + "_*").toArray(new String[]{});
+
+        return redisTemplate.opsForHash().keys(groupName).toArray(new String[]{});
+
     }
 }
